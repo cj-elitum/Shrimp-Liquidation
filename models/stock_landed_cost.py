3691a00c2e998377ddeaef8202e9fe9faff7d565
Johnny Coral <jcoral@elitumgroup.com>
2023-03-08 10:35:30 -0500

    Agregue campo de costes adicionales en stock.landing

--- models/stock_landed_cost.py
+++ models/stock_landed_cost.py
@@ -4,4 +4,9 @@ class StockLandedCost(models.Model):
     _inherit = 'stock.landed.cost'
 
     liquidation_id = fields.Many2one('shrimp_liquidation.liquidation', string="Liquidación", ondelete='cascade')
+    landed_cost_total = fields.Float(string="Costo adicional total", compute="_compute_landed_cost_total", store=True, readonly=True)
 
+    @api.depends('valuation_adjustment_lines')
+    def _compute_landed_cost_total(self):
+        for record in self:
+            record.landed_cost_total = sum(record.valuation_adjustment_lines.mapped('additional_landed_cost'))
